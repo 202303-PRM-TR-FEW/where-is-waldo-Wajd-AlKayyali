2051f12a96bf6729e373e42b7577e0dedb039bf0
wajd alkayyali <wajd.kayyali@gmail.com>
2023-10-13 11:26:13 +0300

    Done

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
-
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["P", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+];
 const whereIsWaldo = (matrix) => {
-  
-}
+  const waldo = matrix[0][0];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] !== waldo) {
+        let diffarr = [i + 1, j + 1]
+        console.log(diffarr)
+        return diffarr;
+      }
+    }
+  }
+  return waldo;
+};
+whereIsWaldo(example);
+
+
+
+
+
+
+
