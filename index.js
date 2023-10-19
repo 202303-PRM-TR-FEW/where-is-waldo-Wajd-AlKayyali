6d99ee84e10983868294c19c8ab8889fde799e65
wajd alkayyali <wajd.kayyali@gmail.com>
2023-10-19 15:36:18 +0300

    Dealt with an edge case here

--- index.js
+++ index.js
@@ -11,18 +11,43 @@ const whereIsWaldo = (matrix) => {
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j] !== waldo) {
-        let diffarr = [i + 1, j + 1]
-        console.log(diffarr)
-        return diffarr;
+        let solution = [i + 1, j + 1]
+        console.log(solution)
+        return solution;
+      }
+      else if (
+        (matrix[0][1] !== waldo && matrix[0][2] !== waldo) ||
+        (matrix[0][2] === undefined && matrix[1][1] !== waldo)
+      ) {
+        //Dealt with my edge case here.
+        let solution = [i + 1, j + 1];
+        return solution;
       }
     }
   }
-  return waldo;
 };
 whereIsWaldo(example);
 
 
 
+// function wheresWaldo(array) {
+//   const first = array[0][0];
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array[i].length; j++) {
+//       if (array[i][j] !== first) {
+//         let solution = [i + 1, j + 1];
+//         return solution;
+//       } else if (
+//         (array[0][1] !== first && array[0][2] !== first) ||
+//         (array[0][2] === undefined && array[1][1] !== first)
+//       ) {
+//         //Dealt with my edge case here.
+//         let solution = [i + 1, j + 1];
+//         return solution;
+//       }
+//     }
+//   }
+// }
 
 
 
